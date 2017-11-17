683d2437f6913f22e8a50cfa761d8fe7e1034a3a
chepkoy <allankiplangat22@gmail.com>
2017-11-17 19:13:32 +0300

    Working with graphs in tensorflow

---
+++ graph2.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+a = tf.constant(6.5, name='constant_a')
+
+b = tf.constant(3.4, name='constant_b')
+c = tf.constant(3.0, name='constant_c')
+
+d = tf.constant(100.2, name='constant_d')
+
+square = tf.square(a, name="Square_a")
+power = tf.pow(b, c, name="pow_b_c")
+sqrt = tf.sqrt(d, name="sqrt_d")
+
+final_sum = tf.add_n([square, power, sqrt], name="final_sum")
+
+sess = tf.Session()
+
+print("square of a: ", sess.run(square))
+print("power of b ^ c: ", sess.run(power))
+print("square root of d: ", sess.run(sqrt))
+
+print("Sum of square, power and square root: ", sess.run(final_sum))
+another_sum = tf.add_n([a,b,c,d, power], name="another_sum")
+
+writer = tf.summary.FileWriter('./events2', sess.graph)
+writer.close()
+sess.close()
+
